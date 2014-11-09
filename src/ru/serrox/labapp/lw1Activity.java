351b23a8f44af41c92001656f8a73eb8c270dc59
serrox <serrox@ya.ru>
2014-11-09 17:32:17 +0300

    [lw1] added: menu button, color randomization. finished lab1.

--- src/ru/serrox/labapp/lw1Activity.java
+++ src/ru/serrox/labapp/lw1Activity.java
@@ -2,7 +2,12 @@ package ru.serrox.labapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.GridLayout;
+
+import java.util.Random;
 
 /**
  * Created by serrox on 09.11.2014.
@@ -12,7 +17,28 @@ public class lw1Activity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lw1_layout);
-        GridLayout Layout = (GridLayout) findViewById(R.id.grLay);
-       // Layout.
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.lw1m, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.lw1_upd:
+                // TODO: обработчик нажатия здесь
+                Random rnd = new Random();
+                ((FrameLayout)findViewById(R.id.lw1_bc)).setBackgroundColor(rnd.nextInt());
+                ((FrameLayout)findViewById(R.id.lw1_bl)).setBackgroundColor(rnd.nextInt());
+                ((FrameLayout)findViewById(R.id.lw1_cc)).setBackgroundColor(rnd.nextInt());
+                ((FrameLayout)findViewById(R.id.lw1_cr)).setBackgroundColor(rnd.nextInt());
+                ((FrameLayout)findViewById(R.id.lw1_tl)).setBackgroundColor(rnd.nextInt());
+                ((FrameLayout)findViewById(R.id.lw1_tr)).setBackgroundColor(rnd.nextInt());
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }

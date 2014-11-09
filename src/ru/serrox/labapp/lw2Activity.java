75f92c9d81d3d6a9cf48bd7d35fe6268e525419e
serrox <serrox@ya.ru>
2014-11-09 19:57:10 +0300

    [lw2] implement deletion of notes

--- src/ru/serrox/labapp/lw2Activity.java
+++ src/ru/serrox/labapp/lw2Activity.java
@@ -2,11 +2,9 @@ package ru.serrox.labapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.*;
 
 import java.util.ArrayList;
 
@@ -20,16 +18,28 @@ public class lw2Activity extends Activity {
         setContentView(R.layout.lw2_layout);
 
         final ArrayList<String> items = new ArrayList<String>();
-        final ArrayAdapter<String> a = new ArrayAdapter<String>(this,R.layout.lw2_item_layout,R.id.lw2_tv_obj,items);
-        ((ListView)findViewById(R.id.lw2_listView)).setAdapter(a);
+        final ArrayAdapter<String> a = new ArrayAdapter<String>(this, R.layout.lw2_item_layout, R.id.lw2_tv_obj, items);
+        ListView listv = (ListView) findViewById(R.id.lw2_listView);
+        listv.setAdapter(a);
 
         ((Button) findViewById(R.id.lw2_add_b)).setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                   a.add(((EditText)findViewById(R.id.lw2_input)).getText().toString());
-                   a.notifyDataSetChanged();
-                   ((EditText)findViewById(R.id.lw2_input)).setText("");
-             }
+            @Override
+            public void onClick(View v) {
+                a.add(((EditText) findViewById(R.id.lw2_input)).getText().toString());
+                a.notifyDataSetChanged();
+                ((EditText) findViewById(R.id.lw2_input)).setText("");
+            }
         });
+
+        listv.setLongClickable(true);
+
+        listv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                a.remove(a.getItem(position));
+                return true;
+            }
+        });
+
+
     }
 }

13ec475ad748f489c27a90d832ded17e1095dfc3
serrox <serrox@ya.ru>
2014-11-09 21:09:39 +0300

    [lw3]start working on lab#3

--- src/ru/serrox/labapp/MainActivity.java
+++ src/ru/serrox/labapp/MainActivity.java
@@ -30,6 +30,14 @@ public class MainActivity extends Activity {
                 startActivity(intent);
             }
         });
+        Button go_lw3 = (Button) findViewById(R.id.butlw3);
+        go_lw3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, lw3Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
---
+++ src/ru/serrox/labapp/lw3Activity.java
@@ -0,0 +1,15 @@
+package ru.serrox.labapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by serrox on 09.11.2014.
+ */
+public class lw3Activity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lw3_layout);
+    }
+}
